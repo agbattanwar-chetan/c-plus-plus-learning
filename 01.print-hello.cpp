27467883afb7d7d832a58cc60f7a9fa82171c461
chetan <chetanagbattanwar26@gmail.com>
2025-06-05 19:41:28 +0530

    print hello world

---
+++ 01.print-hello.cpp
@@ -0,0 +1,82 @@
+// #include <iostream>
+// int main() { std::cout << "Hello World!\n"; }
+
+//  or
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
+  }
+
+// ---x---x----x----x----x----
+
+// #include <iostream>
+// int main() {
+//   std::cout << "Hello World!";
+//   std::cout << std::endl;
+//   std::cout << "Welcome dosto";
+// }
+
+// or
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   cout << "Hello World!";
+//   cout << endl;
+//   cout << "Welcome dosto";
+// }
+
+// or
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   cout << "Hello World!" << endl << "Welcome dosto";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   cout << "Hello World!";
+//   cout << '\n';
+//   cout << "Welcome dosto";
+// }
+
+// or
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   cout << "Hello World!\nWelcome dosto";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int n;
+//   cout << "Enter a number: ";
+//   cin >> n;
+//   cout << "The value of n is " << n;
+// }
+
+// homework 1
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   cout << "My name is CHETAN" << endl
+//        << "My GOAL is to join FAANG" << endl
+//        << "I will stick to my goal.";
+// }
+
+//  homework 2
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int n;
+//   cout<< "Enter number of subjects you like: ";
+//   cin>> n;
+//   cout<<"You have entered : "<< n << endl << "You like " << n << "
+//   subjects.";
+// }
